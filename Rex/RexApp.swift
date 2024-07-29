a79c4951fd45acc2236c83fd3b65323c935a6990
Miclaine Emtman <clainie@gmail.com>
2024-07-29 16:41:32 -0700

    Initial Commit

---
+++ Rex/RexApp.swift
@@ -0,0 +1,17 @@
+//
+//  RexApp.swift
+//  Rex
+//
+//  Created by Miclaine Emtman on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct RexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
